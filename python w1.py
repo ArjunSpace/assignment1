e84aff275cf38684de3fec0abd71ca82ca3cba82
ArjunSpace <117995211+ArjunSpace@users.noreply.github.com>
2022-11-16 11:15:03 +0530

    Update python w1.py

--- python w1.py
+++ python w1.py
@@ -47,15 +47,26 @@ Q8. What is type casting?
 
 Q9. Can we take more than one input from the user using single input() function? If yes, how? If no, why?
 
+	yes, taking multiple inputs is possible with the input() function by using split method or list comprehension method
+    Ex: # taking three inputs at a time
+	x, y, z = input("Enter three values: ").split()
+	print("Total number of students: ", x)
+	print("Number of boys is : ", y)
+	print("Number of girls is : ", z)
+	
+
 	
 Q10. What are keywords?
 
 	key words in python are reserved words that can not be used as a variable name, function name,
      or any other identifier.	
-     Ex: True,False,if,else etc..
+     Ex: True,False,if,else etc..	
 
 Q11. Can we use keywords as a variable? Support your answer with reason.
 
+	Keywords are predefined, reserved words used in Python programming that have special meanings to the compiler.
+
+We cannot use a keyword as a variable name, function name, or any other identifier. They are used to define the syntax and structure of the Python language.
 
 Q12. What is indentation? What's the use of indentaion in Python?
 
@@ -94,6 +105,22 @@ if int_num % 2 == 0:
     print(int_num," is Even")
 else:
     print(int_num, " is odd" ) 
+Q18. What are boolean operator?
+
+ 	 The opearators representing or gives an idea wheather it is true or false to an condition, the boolean key words are:
+		"True","False"
+	
+Q19. What will the output of the following?
+```
+1 or 0  >>>> True
+
+0 and 0  >>>> False
+
+True and False and True >>> False
+
+1 or 0 or 0  >>>> True
+
+Q20. What are conditional statements in Python?
 
 Q22. Write a code to take the age of person as an input and if age >= 18 display "I can vote". If age is < 18 display "I can't vote".
 
